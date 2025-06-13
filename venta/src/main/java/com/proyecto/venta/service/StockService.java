a46a159e2bea269f622e7e21dc020328a339cbeb
ren033 <107058457+ren033@users.noreply.github.com>
2025-06-13 20:02:41 -0300

    commit

--- venta/src/main/java/com/proyecto/venta/service/StockService.java
+++ venta/src/main/java/com/proyecto/venta/service/StockService.java
@@ -3,6 +3,10 @@ package com.proyecto.venta.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StockService {
-    
+public class StockService
+{
+    @Autowired
+    private StockService stockService;
+
+    public List<Stock> listStock()
 }
