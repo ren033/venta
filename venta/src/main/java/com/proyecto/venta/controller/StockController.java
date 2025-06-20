85b354c833f5effa435882469994684f0e8840d4
ren033 <107058457+ren033@users.noreply.github.com>
2025-06-20 17:12:49 -0300

    commit

--- venta/src/main/java/com/proyecto/venta/controller/StockController.java
+++ venta/src/main/java/com/proyecto/venta/controller/StockController.java
@@ -16,6 +16,7 @@ public class StockController
 {
     @Autowired
     private StockService stockService;
+
     @GetMapping
     public ResponseEntity<List<Stock>> getAll()
     {
@@ -25,5 +26,38 @@ public class StockController
         } else {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Stock> getById(@PathVariable int id){
+        return new ResponseEntity<Stock>(stockService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity <Stock> postStock(@RequestBody Stock stock)
+    {
+        Stock search = stockService.findById(stock.getId());
+        if (search == null) {
+            return new ResponseEntity<>(stockService.save(stock), HttpStatus.CREATED);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @DeleteMapping("/id")
+    public void deleteStock(@PathVariable int id){
+        stockService.deleteById(id);
+    }
+
+    @PutMapping("id")
+    public ResponseEntity<Stock> updateStock(@RequestBody Stock stock, @PathVariable int id)
+    {
+        List<Stock> updateStock = stockService.listStock();
+        if (!updateStock.isEmpty())
+            return ResponseEntity.notFound().build();
+            stock.setId(id);
+            stockService.save(stock);
+            return ResponseEntity.noContent().build();
     }
 }
