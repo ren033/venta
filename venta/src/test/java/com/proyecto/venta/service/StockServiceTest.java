7425aa5c7374906dcda36aafbd69ca19f2100aed
ren033 <107058457+ren033@users.noreply.github.com>
2025-06-21 15:53:09 -0300

    commit

--- venta/src/test/java/com/proyecto/venta/service/StockServiceTest.java
+++ venta/src/test/java/com/proyecto/venta/service/StockServiceTest.java
@@ -1,7 +1,11 @@
 package com.proyecto.venta.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,17 +29,31 @@ class StockServiceTest {
     }
 
     @Test
+    void testListStock() {
+        Stock s1 = new Stock(1, "Market M", "Audifinos", 12, true);
+        Stock s2 = new Stock(2, "Market M", "Cargador", 0, false);
+        Stock s3 = new Stock(3, "Market M", "Celular", 7, true);
+        Stock s4 = new Stock(4, "TechStock", "Cargador", 10, true);
+
+        when(stockRepository.findAll()).thenReturn(Arrays.asList(s1, s2, s3, s4));
+
+        List<Stock> resultado = stockService.listStock();
+        assertThat(resultado).hasSize(4).contains(s1, s2, s3, s4);
+        verify(stockRepository).findAll();
+    }
+
+    @Test
     public void testSaveStock() {
-        Stock stock = new Stock(1, "Market Local",  "Audifonos", 4, true);
+        Stock stock = new Stock(1, "Market M",  "Audifonos", 12, true);
 
         when(stockRepository.save(stock)).thenReturn(stock);
         Stock saved = stockService.save(stock);
 
         assertNotNull(saved);
         assertEquals(1, saved.getId());
-        assertEquals("Market Local", saved.getTienda());
+        assertEquals("Market M", saved.getTienda());
         assertEquals("Audifonos", saved.getProducto());
-        assertEquals(4, saved.getCantidad());
+        assertEquals(12, saved.getCantidad());
         verify(stockRepository).save(stock);
     }
 }
